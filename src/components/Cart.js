59893340b3eb953facd8e777aa195f4495308d03
Gustavo Silva <Gustavo Silva>
2022-05-15 21:12:46 -0300

    se añadio la funcionalidad al carrito y conteo de pedidos

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 //CALL MODULES
 import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 //IMPORTING CONTEXT
 import { OrderContext } from './context/CartContext';
@@ -23,43 +24,50 @@ export const Cart = () => {
 
     //RENDERING COMPONENT
     return (
-        <div className='cartContainer'>
+        <div className='cartContainer mb-5'>
             <div className="row">
-                <h1 className='display-4 cartTitle'>Shopping Cart</h1>
+                <h1 className='display-3 cartTitle'>Shopping Cart</h1>
 
-                <div className="col-md-9">
-                    {orders.orders.map(orden => (
-                        <CartProduct key={orden.id} orden={orden} />
-                    ))}
-                </div>
-                <div className="col-md-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="finalPriceDetails">
-                                <i className="fa-solid fa-cart-arrow-down"></i>
-                                <div className="priceDetails__container">
-                                    <p className="priceContainer__final">Precio Final</p>
-                                    <p className="priceContainer__total">USD {Math.round((orders.totalPrice + Number.EPSILON)*100)/100}</p>
-                                    <p className="priceContainer__taxes">Incluye Tasas e Impuestos</p>
+                {(orders.orders.length > 0) ? 
+                    <>
+                        <div className="col-md-9">
+                            {orders.orders.map(orden => (
+                                <CartProduct key={orden.id} orden={orden} />
+                            ))}
+                        </div>
+                        <div className="col-md-3">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="finalPriceDetails">
+                                        <i className="fa-solid fa-cart-arrow-down"></i>
+                                        <div className="priceDetails__container">
+                                            <p className="priceContainer__final">Precio Final</p>
+                                            <p className="priceContainer__total">USD {Math.round((orders.totalPrice + Number.EPSILON)*100)/100}</p>
+                                            <p className="priceContainer__taxes">Incluye Tasas e Impuestos</p>
+                                        </div>
+                                    </div>
+                                    <p className="mt-3 passangersNumber__paragraph">Cantidad Productos {orders.totalAmount}</p>
+                                    <div className="flightsPriceContainer">
+                                        <div className="subtotalContainer">
+                                            <p className="subtotalContainer__flights">Valor pedidos</p>
+                                            <p className="subtotalContainer__subt">USD ${Math.round(((orders.totalPrice - orders.totalIva) + Number.EPSILON)*100)/100}</p>
+                                        </div>
+                                        <div className="taxesContainer">
+                                            <p className="taxesContainer__taxDetail">Tasas e Impuestos</p>
+                                            <p className="taxesContainer__taxMoney">USD ${Math.round((orders.totalIva + Number.EPSILON)*100)/100}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <p className="mt-3 passangersNumber__paragraph">Cantidad Productos {orders.totalAmount}</p>
-                            <div className="flightsPriceContainer">
-                                <div className="subtotalContainer">
-                                    <p className="subtotalContainer__flights">Valor pedidos</p>
-                                    <p className="subtotalContainer__subt">USD ${Math.round(((orders.totalPrice - orders.totalIva) + Number.EPSILON)*100)/100}</p>
-                                </div>
-                                <div className="taxesContainer">
-                                    <p className="taxesContainer__taxDetail">Tasas e Impuestos</p>
-                                    <p className="taxesContainer__taxMoney">USD ${Math.round((orders.totalIva + Number.EPSILON)*100)/100}</p>
+                                <div className="d-grid mt-3 p-2">
+                                    <button className='btn btn-primary btn-lg text-center'>Pay Order</button>
                                 </div>
                             </div>
                         </div>
-                        <div className="d-grid mt-3 p-2">
-                            <button className='btn btn-primary btn-lg text-center'>Pay Order</button>
-                        </div>
+                    </> : <div className='text-center mb-5'>
+                        <h2 className='display-4 fw-bold emptyCartContainer__title'>Carrito vacio, redirigase a la ventana de productos y empiece a comprar!!</h2>
+                        <Link to='/' className='btn btn-dark btn-lg mt-3'>Comprar Productos</Link>
                     </div>
-                </div>
+                }
             </div>
         </div>
     )
--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,12 +1,31 @@
 //CALL MODULES AND METHODS
+import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+//IMPORTING CONTEXT
+import { OrderContext } from './context/CartContext';
 
 //FUNCTIONS AND METHODS
 export const CartWidget = () => {
+    //USE CONTEXT - FUNCTIONS AND VALUES
+    const { initialState } = useContext(OrderContext);
+
+    //STATES
+    const [cantidadPedida, setCantidadPedida] = useState(initialState);
+
+    //USEEFFECT SHOW DATA WHEN I LOAD (Piece of code wich runs based on a condition)
+    //Getting data from context
+    useEffect(() => {
+        setCantidadPedida(initialState);
+    }, [initialState]);
+
     //RENDERING ICON COMPONENT
     return (
-        <li className="linksMenu__disappear">
-            <Link to="/cart"><i className="fas fa-shopping-cart"></i></Link>
+        <li className={`linksMenu__disappear ${(cantidadPedida.totalAmount <= 0) && 'cartIconContainer'}`}>
+            <Link to="/cart" className='cartIconContainer__link'>
+                <i className="fas fa-shopping-cart"></i>
+                <p className='iconContainer__number'>{cantidadPedida.totalAmount}</p>
+            </Link>
         </li>
     )
 };
